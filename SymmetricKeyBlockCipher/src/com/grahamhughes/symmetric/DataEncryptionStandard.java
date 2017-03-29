8226b562d374ff4c4bce49ba6d94efc074092b4e
graham-hughes <grahamhughes@Grahams-MacBook-Pro.local>
2017-03-29 11:16:56 -0700

    Started block encryption, finished up to writing functionF

--- SymmetricKeyBlockCipher/src/com/grahamhughes/symmetric/DataEncryptionStandard.java
+++ SymmetricKeyBlockCipher/src/com/grahamhughes/symmetric/DataEncryptionStandard.java
@@ -11,6 +11,24 @@ import java.util.BitSet;
  */
 public class DataEncryptionStandard {
     Keys keys;
+    private int[] IP = {58,  50, 42, 34, 26, 18, 10, 2,
+            60, 52, 44, 36, 28, 20, 12, 4,
+            62, 54, 46, 38, 30, 22, 14, 6,
+            64, 56, 48, 40, 32, 24, 16, 8,
+            57, 49, 41, 33, 25, 17, 9, 1,
+            59, 51, 43, 35, 27, 19, 11, 3,
+            61, 53, 45, 37, 29, 21, 13, 5,
+            63, 55, 47, 39, 31, 23, 15, 7};
+
+    private int[] E = {32, 1, 2, 3, 4, 5,
+                    4, 5, 6, 7, 8, 9,
+                    8, 9, 10, 11, 12, 13,
+                    12, 13, 14, 15, 16, 17,
+                    16, 17, 18, 19, 20, 21,
+                    20, 21, 22, 23, 24, 25,
+                    24, 25, 26, 27, 28, 29,
+                    28, 29, 30, 31, 32, 1};
+
     public DataEncryptionStandard(String k) {
         keys = new Keys(k);
     }
@@ -35,5 +53,44 @@ public class DataEncryptionStandard {
         return output;
     }
 
+    /* Not finished */
+    public String encrypt(String str) {
+        ArrayList<Block> blocks = stringToBlocks(str);
+        String output = "";
+        for (Block b : blocks) {
+            output += encrypt(b);
+        }
+        return output;
+    }
+
+    /* Not finished */
+    private String encrypt(Block block) {
+        BitSet IPLeft = new BitSet(32);
+        BitSet IPRight = new BitSet(32);
+        /* Performs initial permutation and splits into left/right*/
+        for (int i = 0; i < 32; i ++) {
+            IPLeft.set(i, block.getBlock().get(IP[i]));
+        }
+        for (int i = 0; i < 32; i ++) {
+            IPRight.set(i, block.getBlock().get(IP[i + 32]));
+        }
+
+        BitSet IPLeftMinusOne;
+        BitSet IPRightMinusOne;
+        for (int i = 0; i < 16; i++) {
+            IPLeftMinusOne = IPLeft;
+            IPRightMinusOne = IPRight;
+            IPLeft = IPRightMinusOne;
+            IPRight = IPLeftMinusOne;
+            IPRight.xor(functionF(IPRightMinusOne, keys.subKeys[i]));
+        }
+
+
+        return "";
+    }
+
+    private BitSet functionF(BitSet data, BitSet key) {
+        return null;
+    }
 
 }
--- SymmetricKeyBlockCipher/src/com/grahamhughes/symmetric/Keys.java
+++ SymmetricKeyBlockCipher/src/com/grahamhughes/symmetric/Keys.java
@@ -9,10 +9,28 @@ import java.util.BitSet;
  */
 public class Keys {
     BitSet keyPlus;
-    BitSet[] keys;
+    BitSet[] subKeys;
+    private int[] PC1 = {57, 49, 41, 33, 25, 17, 9,
+                        1, 58, 50, 42, 34, 26, 18,
+                        10, 2, 59, 51, 43, 35, 27,
+                        19, 11, 3, 60, 52, 44, 36,
+                        63, 55, 47, 39, 31, 23, 15,
+                        7, 62, 54, 46, 38, 30, 22,
+                        14, 6, 61, 53, 45, 37, 29,
+                        21, 13, 5, 28, 20, 12, 4};
+      
+    private int[] PC2 = {14, 17, 11, 24,  1, 5,
+                        3, 28, 15, 6, 21, 10,
+                        23, 19, 12, 4, 26, 8,
+                        16, 7, 27, 20, 13, 2,
+                        41, 52, 31, 37, 47, 55,
+                        30, 40, 51, 45, 33, 48,
+                        44, 49, 39, 56, 34, 53,
+                        46, 42, 50, 36, 29, 32};
+
 
     public Keys(String k) {
-        keys = new BitSet[16];
+        subKeys = new BitSet[16];
         BitSet[] subKeysC;
         BitSet[] subKeysD;
         BitSet[] subKeysK = new BitSet[16];
@@ -23,20 +41,18 @@ public class Keys {
         BitSet temp = BitSet.valueOf(k.getBytes());
         keyPlus = new BitSet(56);
 
-        int j = 65;
+        // Initial permutation that also removes the 8 parity bits
         for (int i = 0; i < 56; i ++) {
-            j -= 8;
-            if (j < 0) {
-                j += 65;
-            }
-            keyPlus.set(i, temp.get(j));
+            keyPlus.set(i, temp.get(PC1[i]));
         }
+
         BitSet cNaught = keyPlus.get(0, 28);
         BitSet dNaught = keyPlus.get(28, 56);
 
         subKeysC = subKeysPC1(cNaught);
         subKeysD = subKeysPC1(dNaught);
 
+        /* Concatenates C and D keys into subKeysK*/
         for (int i = 0; i < 16; i++) {
             subKeysK[i] = new BitSet(56);
             for (int q = 0; q < 28; q++) {
@@ -47,8 +63,9 @@ public class Keys {
             }
         }
 
+        /* Final operation that permutes subkeysK into the final set of subkeys */
         for (int i = 0; i < 16; i++) {
-            keys[i] = subKeysPC2(subKeysK[i]);
+            subKeys[i] = subKeysPC2(subKeysK[i]);
         }
     }
 
@@ -67,66 +84,12 @@ public class Keys {
         return sub;
     }
 
-    /* Helper method for generating a subkey for PC2
-    *  This is ugly, but I couldn't find any justification better than a table */
+    /* Helper method for generating a subkey for PC2 */
     private BitSet subKeysPC2(BitSet k) {
         BitSet next = new BitSet(48);
-        next.set(0, k.get(14));
-        next.set(1, k.get(17));
-        next.set(2, k.get(11));
-        next.set(3, k.get(24));
-        next.set(4, k.get(1));
-        next.set(5, k.get(5));
-
-        next.set(6, k.get(3));
-        next.set(7, k.get(28));
-        next.set(8, k.get(15));
-        next.set(9, k.get(6));
-        next.set(10, k.get(21));
-        next.set(11, k.get(10));
-
-        next.set(12, k.get(23));
-        next.set(13, k.get(19));
-        next.set(14, k.get(12));
-        next.set(15, k.get(4));
-        next.set(16, k.get(26));
-        next.set(17, k.get(8));
-
-        next.set(18, k.get(16));
-        next.set(19, k.get(7));
-        next.set(20, k.get(27));
-        next.set(21, k.get(20));
-        next.set(22, k.get(13));
-        next.set(23, k.get(2));
-
-
-        next.set(24, k.get(41));
-        next.set(25, k.get(52));
-        next.set(26, k.get(31));
-        next.set(27, k.get(37));
-        next.set(28, k.get(47));
-        next.set(29, k.get(55));
-
-        next.set(30, k.get(30));
-        next.set(31, k.get(40));
-        next.set(32, k.get(51));
-        next.set(33, k.get(45));
-        next.set(34, k.get(33));
-        next.set(35, k.get(48));
-
-        next.set(36, k.get(44));
-        next.set(37, k.get(49));
-        next.set(38, k.get(39));
-        next.set(39, k.get(56));
-        next.set(40, k.get(34));
-        next.set(41, k.get(53));
-
-        next.set(42, k.get(46));
-        next.set(43, k.get(42));
-        next.set(44, k.get(50));
-        next.set(45, k.get(36));
-        next.set(46, k.get(29));
-        next.set(47, k.get(32));
+        for (int i = 0; i < 48; i++) {
+            next.set(0, k.get(PC2[i]));
+        }
         return next;
     }
 
